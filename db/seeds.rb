4474f2a2e567fb2c4c3edb0af40a2ac0b2c760d1
yhuzued <yusufmait@gmail.com>
2022-12-07 14:10:22 +0700

    seeding flight

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Airport.create [{name: "Jakarta"}, {name: "Bali"}]
+array_of_airport = [Airport.first, Airport.last]
+
+1000.times do |i| 
+  origin = array_of_airport.sample
+
+  destination = Airport.first if origin == Airport.last
+  destination = Airport.last if origin == Airport.first
+
+  make_flight = origin.origin.build(start: DateTime.now + 1, flight_duration: 4, destination: destination)
+  make_flight.save
+end
