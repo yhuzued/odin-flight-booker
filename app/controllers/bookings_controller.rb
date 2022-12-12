36d9cbb8fff3387f9115d37efe21ca0d7e818b0a
yhuzued <yusufmait@gmail.com>
2022-12-12 08:14:39 +0700

    Create new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,26 @@
 class BookingsController < ApplicationController
-  
-  def index
-    
-  end
-
   def new
-    
+    @flight = Flight.find(params["booking"][:flight_id])
+    @booking = @flight.bookings.build(booking_params)
   end
 
   def create
-   
+    @flight = Flight.find(params["booking"][:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to root_path
+    else
+      # handle error
+      logger.error "Error saving booking: #{@booking.errors.full_messages.join(', ')}"
+    end
   end
 
-  private 
+  private
 
-  def allowed_post_params
-   
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
-
+  
+  
 end
--- app/controllers/passanger_controller.rb
+++
@@ -1,8 +0,0 @@
-class PassangerController < ApplicationController
-  def new
-  end
-
-  def create
-    redirect_to root_path
-  end
-end
