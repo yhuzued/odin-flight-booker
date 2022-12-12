e7410b6e9c8a0542c1ae8b941145ca2d3e6f10fe
yhuzued <yusufmait@gmail.com>
2022-12-12 13:38:37 +0700

    Add dependent

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
- belongs_to :flight
- has_many :passengers
- accepts_nested_attributes_for :passengers
+  belongs_to :flight
+  has_many :passengers, dependent: :destroy
+  accepts_nested_attributes_for :passengers
 end
